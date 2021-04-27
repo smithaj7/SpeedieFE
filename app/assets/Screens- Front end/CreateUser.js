218f1a6c1b817c079e8be748baf94d960a725a26
smithaj7 <54448709+smithaj7@users.noreply.github.com>
2021-04-27 12:14:09 -0500

    Password fix

--- app/assets/Screens- Front end/CreateUser.js
+++ app/assets/Screens- Front end/CreateUser.js
@@ -81,10 +81,8 @@ export default class CreateUser extends React.Component {
         var email = params.user;
         var loc = params.location;
         var employeeRole = params.role;
-        console.log("Email: ", email)
-        console.log("Loc: ", loc)
-        console.log("Role: ", employeeRole)
-        this.props.navigation.navigate("LandingPage", {user: email, location: loc, role: employeeRole});
+        var passwordLength = params.pLength;
+        this.props.navigation.navigate("LandingPage", {user: email, location: loc, role: employeeRole, pLength: passwordLength});
         // }
         }
       }
