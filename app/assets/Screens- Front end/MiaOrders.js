69057c1353ae58addb5a2130aa011904eaa5e2e9
henrylgoldenberg <hgoldenberg@me.com>
2021-04-27 10:33:28 -0500

    Menu View update

--- app/assets/Screens- Front end/MiaOrders.js
+++ app/assets/Screens- Front end/MiaOrders.js
@@ -720,8 +720,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flexDirection: "row",
     marginTop: 20,
-    height: "10%",
-    width: "100%",
+    height: "55px",
+    width: "100vw",
     backgroundColor: "#093b15"
   },
   leftButton: {
